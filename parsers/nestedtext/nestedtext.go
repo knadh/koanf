cc013d8673118a47d889faa81289de3e3534ee24
Norbert Pillmayer <norbert@pillmayer.com>
2021-11-16 09:34:40 +0100

    Wrapped npillmayer/nestext into a NestedText Parser

---
+++ parsers/nestedtext/nestedtext.go
@@ -0,0 +1,41 @@
+// Package nestedtext implements a koanf Parser that parses NestedText bytes as conf maps.
+package nestedtext
+
+import (
+	"bytes"
+
+	"github.com/npillmayer/nestext"
+	"github.com/npillmayer/nestext/ntenc"
+)
+
+// NT implements a NestedText parser.
+type NT struct{}
+
+// Parser returns a NestedText Parser.
+func Parser() *NT {
+	return &NT{}
+}
+
+// Unmarshal parses the given NestedText bytes.
+//
+// If the NT content does not reflect a dict (NT allows for top-level lists or strings as well),
+// the content will be wrapped into a dict with a single key named "dict".
+func (p *NT) Unmarshal(b []byte) (map[string]interface{}, error) {
+	r := bytes.NewReader(b)
+	result, err := nestext.Parse(r, nestext.TopLevel("dict"))
+	if err != nil {
+		return nil, err
+	}
+	return result.(map[string]interface{}), nil
+
+}
+
+// Marshal marshals the given config map to NestedText bytes.
+func (p *NT) Marshal(m map[string]interface{}) ([]byte, error) {
+	buf := &bytes.Buffer{}
+	_, err := ntenc.Encode(m, buf)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
