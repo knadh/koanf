1fe479942564bda4578da0f5de8679a694f57963
mkvolkov <mk.volkov34@gmail.com>
2022-07-09 20:21:32 +0300

    Checking added

--- examples/read-consul/main.go
+++ examples/read-consul/main.go
@@ -6,11 +6,25 @@ import (
 
 	"github.com/knadh/koanf"
 	"github.com/knadh/koanf/providers/consul"
+	"github.com/hashicorp/consul/api"
 )
 
 var k = koanf.New(".")
 
 func main() {
+	cli, err := api.NewClient(api.DefaultConfig())
+	if err != nil {
+		panic(err)
+	}
+
+	kv := cli.KV()
+
+	newPair := &api.KVPair{Key: "parent1", Value: []byte("father")}
+	_, err = kv.Put(newPair, nil)
+	if err != nil {
+		panic(err)
+	}
+
 	provider := consul.Provider(consul.Config {
 		Key: "parent1",
 		Recurse: false,
