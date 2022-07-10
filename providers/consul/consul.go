ac7ec60d4b23daf396f7dba72f6bccb203271eab
mkvolkov <mk.volkov34@gmail.com>
2022-07-10 14:09:00 +0300

    Detailed option

--- providers/consul/consul.go
+++ providers/consul/consul.go
@@ -2,6 +2,7 @@ package consul
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/hashicorp/consul/api"
 )
@@ -42,29 +43,55 @@ func (cProvider *CProvider) Read() (map[string]interface{}, error) {
 	kv := cProvider.client.KV()
 
 	if cProvider.cfg.Recurse {
-		if cProvider.cfg.Detailed {
-			// TODO: detailed info
-			return nil, nil
-		} else {
-			pairs, _, err := kv.List(cProvider.cfg.Key, nil)
-			if err != nil {
-				return nil, err
-			}
+		pairs, _, err := kv.List(cProvider.cfg.Key, nil)
+		if err != nil {
+			return nil, err
+		}
 
+		if cProvider.cfg.Detailed {
+			for _, pair := range pairs {
+				key_meta := make(map[string]interface{})
+				key_meta["CreateIndex"] = fmt.Sprintf("%d", pair.CreateIndex)
+				key_meta["Flags"] = fmt.Sprintf("%d", pair.Flags)
+				key_meta["LockIndex"] = fmt.Sprintf("%d", pair.LockIndex)
+				key_meta["ModifyIndex"] = fmt.Sprintf("%d", pair.ModifyIndex)
+				if pair.Session == "" {
+					key_meta["Session"] = "-"
+				} else {
+					key_meta["Session"] = fmt.Sprintf("%s", pair.Session)
+				}
+
+				key_meta["Value"] = string(pair.Value)
+
+				mp[pair.Key] = key_meta
+			}
+		} else {
 			for _, pair := range pairs {
 				mp[pair.Key] = string(pair.Value)
 			}
 		}
 	} else {
+		pair, _, err := kv.Get(cProvider.cfg.Key, nil)
+		if err != nil {
+			return nil, err
+		}
+
 		if cProvider.cfg.Detailed {
-			// TODO: detailed info
-			return nil, nil
-		} else {
-			pair, _, err := kv.Get(cProvider.cfg.Key, nil)
-			if err != nil {
-				return nil, err
+			key_meta := make(map[string]interface{})
+			key_meta["CreateIndex"] = fmt.Sprintf("%d", pair.CreateIndex)
+			key_meta["Flags"] = fmt.Sprintf("%d", pair.Flags)
+			key_meta["LockIndex"] = fmt.Sprintf("%d", pair.LockIndex)
+			key_meta["ModifyIndex"] = fmt.Sprintf("%d", pair.ModifyIndex)
+			if pair.Session == "" {
+				key_meta["Session"] = "-"
+			} else {
+				key_meta["Session"] = fmt.Sprintf("%s", pair.Session)
 			}
 
+			key_meta["Value"] = string(pair.Value)
+
+			mp[pair.Key] = key_meta
+		} else {
 			mp[pair.Key] = string(pair.Value)
 		}
 	}
