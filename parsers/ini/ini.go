2c2d7bd42d802c2e3de31db1171ef0da43b9b29b
mkvolkov <mk.volkov34@gmail.com>
2022-08-24 19:20:31 +0300

    unmarshal test

--- parsers/ini/ini.go
+++ parsers/ini/ini.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"gopkg.in/ini.v1"
 	"strings"
+	"time"
 )
 
 type INI struct {}
@@ -24,12 +25,12 @@ func (p *INI) Unmarshal(b []byte) (map[string]interface{}, error) {
 	for _, section := range iniFile.Sections() {
 		if strings.Compare(section.Name(), "DEFAULT") == 0 {
 			for _, key := range section.Keys() {
-				out[key.Name()] = key.Value()
+				out[key.Name()] = convertKey(key)
 			}
 		} else {
 			mpS := make(map[string]interface{}, 0)
 			for _, key := range section.Keys() {
-				mpS[key.Name()] = key.Value()
+				mpS[key.Name()] = convertKey(key)
 			}
 			out[section.Name()] = mpS
 		}
@@ -45,6 +46,14 @@ func (p *INI) Marshal(o map[string]interface{}) ([]byte, error) {
 		switch v.(type) {
 		case map[string]interface{}:
 			continue
+		case bool:
+			s += fmt.Sprintf("%s = %t\n", k, v)
+		case int:
+			s += fmt.Sprintf("%s = %d\n", k, v)
+		case float64:
+			s += fmt.Sprintf("%s = %f\n", k, v)
+		case time.Time:
+			s += fmt.Sprintf("%s = %s\n", k, v.(time.Time).String())
 		default:
 			s += fmt.Sprintf("%s = %s\n", k, v)
 		}
@@ -55,10 +64,46 @@ func (p *INI) Marshal(o map[string]interface{}) ([]byte, error) {
 		case map[string]interface{}:
 			s += fmt.Sprintf("[%s]\n", k)
 			for kData, vData := range v.(map[string]interface{}) {
-				s += fmt.Sprintf("%s = %s\n", kData, vData)
+				switch vData.(type) {
+				case bool:
+					s += fmt.Sprintf("%s = %t\n", kData, vData)
+				case int:
+					s += fmt.Sprintf("%s = %d\n", kData, vData)
+				case float64:
+					s += fmt.Sprintf("%s = %f\n", kData, vData)
+				case time.Time:
+					s += fmt.Sprintf("%s = %s\n", kData, vData.(time.Time).String())
+				default:
+					s += fmt.Sprintf("%s = %s\n", kData, vData)
+				}
 			}
 		}
 	}
 
 	return []byte(s), nil
+}
+
+func convertKey(key *ini.Key) interface{} {
+	b, err := key.Bool()
+	if err == nil {
+		return b
+	}
+
+	n, err := key.Int()
+	if err == nil {
+		return n
+	}
+
+	x, err := key.Float64()
+	if err == nil {
+		return x
+	}
+
+	t, err := key.Time()
+	if err == nil {
+		return t
+	}
+
+	s := key.String()
+	return s
 }
