a1a25ed0441a0134c25c766e86cf6e82b5ecd66c
lyon.nee <lyon.nee@outlook.com>
2023-05-04 11:45:42 +0800

    refactor: mock etcd

--- providers/etcd/etcd_test.go
+++ providers/etcd/etcd_test.go
@@ -4,24 +4,30 @@ import (
 	"github.com/knadh/koanf/v2"
 	"github.com/stretchr/testify/assert"
 	clientv3 "go.etcd.io/etcd/client/v3"
-	"go.etcd.io/etcd/client/v3/mock/mockserver"
+	"go.etcd.io/etcd/server/v3/embed"
 	"testing"
 	"time"
 )
 
 func TestConnect(t *testing.T) {
-	servers, err := mockserver.StartMockServers(1)
+	cfg := embed.NewConfig()
+	cfg.Logger = "zap"
+	cfg.LogLevel = "error"
+	cfg.Dir = "/tmp/test-etcd"
+	cfg.WalDir = ""
+	e, err := embed.StartEtcd(cfg)
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer servers.Stop()
+	defer e.Close()
 
-	address := servers.Servers[0].Address
+	// Create a client connection to the embedded server
+	endpoints := []string{e.Clients[0].Addr().String()}
 
 	provider, err := Provider(Config{
 		ClientConfig: clientv3.Config{
-			Endpoints:   []string{address},
-			DialTimeout: 30 * time.Second,
+			Endpoints:   endpoints,
+			DialTimeout: 5 * time.Second,
 		},
 		Key: "test_key",
 	})
