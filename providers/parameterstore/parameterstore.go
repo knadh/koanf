f8ba6b143c1d411fa5bec0e5ec92374a11e2833e
Taichi Sasaki <tchsskk@gmail.com>
2023-08-30 20:23:28 +0900

    Add parameterstore provider

---
+++ providers/parameterstore/parameterstore.go
@@ -0,0 +1,137 @@
+// Package parameterstore implements a koanf.Provider for AWS Systems Manager Parameter Store.
+package parameterstore
+
+import (
+	"context"
+	"errors"
+
+	awsconfig "github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
+	"github.com/knadh/koanf/maps"
+)
+
+// Input is a constraint that permits any input type
+// to get paramers from AWS Systems Manager Parameter Store.
+type Input interface {
+	ssm.GetParameterInput | ssm.GetParametersInput | ssm.GetParametersByPathInput
+}
+
+// Config represents a ParameterStore provider configuration.
+type Config[T Input] struct {
+	// Delim is the delimiter to use
+	// when specifying config key paths, for instance a . for `parent.child.key`
+	// or a / for `parent/child/key`.
+	Delim string
+
+	// Input is the input to get parameters.
+	Input T
+
+	// OptFns is the additional functional options to get parameters.
+	OptFns []func(*ssm.Options)
+
+	// Callback is an optional callback that takes a (key, value)
+	// with the variable name and value and allows you to modify both.
+	// If the callback returns an empty key, the variable will be ignored.
+	Callback func(key, value string) (string, interface{})
+}
+
+// ParameterStore implements an AWS Systems Manager Parameter Store provider.
+type ParameterStore[T Input] struct {
+	client *ssm.Client
+	config Config[T]
+}
+
+// Provider returns a ParameterStore provider.
+// The AWS Systems Manager Client is configured via environment variables.
+// The configuration values are read from the environment variables.
+//   - AWS_REGION
+//   - AWS_ACCESS_KEY_ID
+//   - AWS_SECRET_ACCESS_KEY
+//   - AWS_SESSION_TOKEN
+func Provider[T Input](config Config[T]) *ParameterStore[T] {
+	c, err := awsconfig.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		return nil
+	}
+	return ProviderWithClient[T](config, ssm.NewFromConfig(c))
+}
+
+// ProviderWithClient returns a ParameterStore provider
+// using an existing AWS Systems Manager client.
+func ProviderWithClient[T Input](config Config[T], client *ssm.Client) *ParameterStore[T] {
+	return &ParameterStore[T]{
+		client: client,
+		config: config,
+	}
+}
+
+// ReadBytes is not supported by the ParameterStore provider.
+func (ps *ParameterStore[T]) ReadBytes() ([]byte, error) {
+	return nil, errors.New("parameterstore provider does not support this method")
+}
+
+// Read returns a nested config map.
+func (ps *ParameterStore[T]) Read() (map[string]interface{}, error) {
+	var (
+		mp = make(map[string]interface{})
+	)
+	switch input := interface{}(ps.config.Input).(type) {
+	case ssm.GetParameterInput:
+		output, err := ps.client.GetParameter(context.TODO(), &input, ps.config.OptFns...)
+		if err != nil {
+			return nil, err
+		}
+		// If there's a transformation callback, run it.
+		if ps.config.Callback != nil {
+			name, value := ps.config.Callback(*output.Parameter.Name, *output.Parameter.Value)
+			// If the callback blanked the key, it should be omitted.
+			if name == "" {
+				break
+			}
+			mp[name] = value
+		} else {
+			mp[*output.Parameter.Name] = *output.Parameter.Value
+		}
+	case ssm.GetParametersInput:
+		output, err := ps.client.GetParameters(context.TODO(), &input, ps.config.OptFns...)
+		if err != nil {
+			return nil, err
+		}
+		for _, p := range output.Parameters {
+			// If there's a transformation callback, run it.
+			if ps.config.Callback != nil {
+				name, value := ps.config.Callback(*p.Name, *p.Value)
+				// If the callback blanked the key, it should be omitted.
+				if name == "" {
+					break
+				}
+				mp[name] = value
+			} else {
+				mp[*p.Name] = *p.Value
+			}
+		}
+	case ssm.GetParametersByPathInput:
+		output, err := ps.client.GetParametersByPath(context.TODO(), &input, ps.config.OptFns...)
+		if err != nil {
+			return nil, err
+		}
+		for _, p := range output.Parameters {
+			// If there's a transformation callback, run it.
+			if ps.config.Callback != nil {
+				name, value := ps.config.Callback(*p.Name, *p.Value)
+				// If the callback blanked the key, it should be omitted.
+				if name == "" {
+					break
+				}
+				mp[name] = value
+			} else {
+				mp[*p.Name] = *p.Value
+			}
+		}
+	}
+	// Unflatten only when a delimiter is specified.
+	if ps.config.Delim != "" {
+		mp = maps.Unflatten(mp, ps.config.Delim)
+	}
+	return mp, nil
+}
---
+++ providers/parameterstore/parameterstore_test.go
@@ -0,0 +1,186 @@
+package parameterstore
+
+import (
+	"context"
+	"strings"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	awsmiddleware "github.com/aws/aws-sdk-go-v2/aws/middleware"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
+	"github.com/aws/aws-sdk-go-v2/service/ssm/types"
+	"github.com/aws/smithy-go/middleware"
+	"github.com/knadh/koanf/v2"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParameterStore(t *testing.T) {
+	c, err := config.LoadDefaultConfig(
+		context.TODO(),
+		config.WithRegion("us-east-1"),
+		config.WithAPIOptions([]func(*middleware.Stack) error{
+			// Mock the SDK response using the middleware.
+			func(stack *middleware.Stack) error {
+				return stack.Finalize.Add(
+					middleware.FinalizeMiddlewareFunc(
+						"Mock",
+						func(ctx context.Context, input middleware.FinalizeInput, handler middleware.FinalizeHandler) (middleware.FinalizeOutput, middleware.Metadata, error) {
+							switch awsmiddleware.GetOperationName(ctx) {
+							case "GetParameter":
+								return middleware.FinalizeOutput{
+									Result: &ssm.GetParameterOutput{
+										Parameter: &types.Parameter{
+											Name:  aws.String("prefix.parent1"),
+											Value: aws.String("alice"),
+										},
+									},
+								}, middleware.Metadata{}, nil
+							case "GetParameters":
+								return middleware.FinalizeOutput{
+									Result: &ssm.GetParametersOutput{
+										Parameters: []types.Parameter{
+											{
+												Name:  aws.String("prefix.parent1"),
+												Value: aws.String("alice"),
+											},
+											{
+												Name:  aws.String("prefix.parent2.child1"),
+												Value: aws.String("bob"),
+											},
+										},
+									},
+								}, middleware.Metadata{}, nil
+							case "GetParametersByPath":
+								return middleware.FinalizeOutput{
+									Result: &ssm.GetParametersByPathOutput{
+										Parameters: []types.Parameter{
+											{
+												Name:  aws.String("prefix.parent1"),
+												Value: aws.String("alice"),
+											},
+											{
+												Name:  aws.String("prefix.parent2.child1"),
+												Value: aws.String("bob"),
+											},
+											{
+												Name:  aws.String("prefix.parent2.child2.grandchild1"),
+												Value: aws.String("carol"),
+											},
+										},
+									},
+								}, middleware.Metadata{}, nil
+							default:
+								return middleware.FinalizeOutput{}, middleware.Metadata{}, nil
+							}
+						},
+					),
+					middleware.Before,
+				)
+			},
+		}),
+	)
+	assert.NoError(t, err)
+	client := ssm.NewFromConfig(c)
+
+	tests := map[string]struct {
+		provider koanf.Provider
+		want     map[string]interface{}
+	}{
+		"get a parameter": {
+			provider: ProviderWithClient(Config[ssm.GetParameterInput]{
+				Delim:    ".",
+				Input:    ssm.GetParameterInput{Name: aws.String("parent1")},
+				Callback: func(key, value string) (string, interface{}) { return strings.TrimPrefix(key, "prefix."), value },
+			}, client),
+			want: map[string]interface{}{
+				"parent1": "alice",
+			},
+		},
+		"get parameters": {
+			provider: ProviderWithClient(Config[ssm.GetParametersInput]{
+				Delim:    ".",
+				Input:    ssm.GetParametersInput{Names: []string{"parent1", "parent2.child1"}},
+				Callback: func(key, value string) (string, interface{}) { return strings.TrimPrefix(key, "prefix."), value },
+			}, client),
+			want: map[string]interface{}{
+				"parent1": "alice",
+				"parent2": map[string]interface{}{
+					"child1": "bob",
+				},
+			},
+		},
+		"get parameters by path": {
+			provider: ProviderWithClient(Config[ssm.GetParametersByPathInput]{
+				Delim:    ".",
+				Input:    ssm.GetParametersByPathInput{Path: aws.String("/")},
+				Callback: func(key, value string) (string, interface{}) { return strings.TrimPrefix(key, "prefix."), value },
+			}, client),
+			want: map[string]interface{}{
+				"parent1": "alice",
+				"parent2": map[string]interface{}{
+					"child1": "bob",
+					"child2": map[string]interface{}{
+						"grandchild1": "carol",
+					},
+				},
+			},
+		},
+		"get a parameter but it is ignored": {
+			provider: ProviderWithClient(Config[ssm.GetParameterInput]{
+				Delim: ".",
+				Input: ssm.GetParameterInput{Name: aws.String("parent1")},
+				Callback: func(key, value string) (string, interface{}) {
+					return strings.TrimPrefix(strings.TrimPrefix(key, "prefix."), "parent1"), value
+				},
+			}, client),
+			want: map[string]interface{}{
+				// Ignored.
+				// "parent1": "alice",
+			},
+		},
+		"get parameters but one is ignored": {
+			provider: ProviderWithClient(Config[ssm.GetParametersInput]{
+				Delim: ".",
+				Input: ssm.GetParametersInput{Names: []string{"parent1", "parent2.child1"}},
+				Callback: func(key, value string) (string, interface{}) {
+					return strings.TrimPrefix(strings.TrimPrefix(key, "prefix."), "parent2.child1"), value
+				},
+			}, client),
+			want: map[string]interface{}{
+				"parent1": "alice",
+				// Ignored.
+				// "parent2": map[string]interface{}{
+				// 	"child1": "bob",
+				// },
+			},
+		},
+		"get parameters by path but ont is ignored": {
+			provider: ProviderWithClient(Config[ssm.GetParametersByPathInput]{
+				Delim: ".",
+				Input: ssm.GetParametersByPathInput{Path: aws.String("/")},
+				Callback: func(key, value string) (string, interface{}) {
+					return strings.TrimPrefix(strings.TrimPrefix(key, "prefix."), "parent2.child2.grandchild1"), value
+				},
+			}, client),
+			want: map[string]interface{}{
+				"parent1": "alice",
+				"parent2": map[string]interface{}{
+					"child1": "bob",
+					// Ignored.
+					// "child2": map[string]interface{}{
+					// 	"grandchild1": "carol",
+					// },
+				},
+			},
+		},
+	}
+	for name, test := range tests {
+		test := test
+		t.Run(name, func(t *testing.T) {
+			got, err := test.provider.Read()
+			assert.NoError(t, err)
+			assert.Equal(t, test.want, got)
+		})
+	}
+}
