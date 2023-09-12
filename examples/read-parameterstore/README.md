8a4cf2a63d6d7e3c4a21ce6927f65c0e82c4124c
Taichi Sasaki <tchssk@users.noreply.github.com>
2023-09-13 02:26:16 +0900

    Add parameterstore provider (#227)

--- examples/read-parameterstore/README.md
+++ examples/read-parameterstore/README.md
@@ -1,4 +1,4 @@
 # AWS Parameter Store Example
 
 ## Link
-[example](https://github.com/defensestation/koanf/blob/main/examples/read-parameterstore/main.go)
+[another example](https://github.com/defensestation/koanf/blob/main/examples/read-parameterstore/main.go)
---
+++ examples/read-parameterstore/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
+	"github.com/aws/aws-sdk-go-v2/service/ssm/types"
+	"github.com/knadh/koanf/providers/parameterstore"
+	"github.com/knadh/koanf/v2"
+)
+
+var k = koanf.New(".")
+
+func main() {
+	// The configuration values are read from the environment variables.
+	c, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+	client := ssm.NewFromConfig(c)
+
+	for k, v := range map[string]string{
+		"parent1":                    "alice",
+		"parent2.child1":             "bob",
+		"parent2.child2.grandchild1": "carol",
+	} {
+		if _, err := client.PutParameter(context.TODO(), &ssm.PutParameterInput{
+			Name:      aws.String(k),
+			Value:     aws.String(v),
+			Type:      types.ParameterTypeSecureString,
+			Overwrite: aws.Bool(true),
+		}); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// Get a parameter.
+	if err := k.Load(parameterstore.ProviderWithClient(parameterstore.Config[ssm.GetParameterInput]{
+		Delim: ".",
+		Input: ssm.GetParameterInput{Name: aws.String("parent1"), WithDecryption: aws.Bool(true)},
+	}, client), nil); err != nil {
+		log.Fatalf("error loading config: %v", err)
+	}
+	fmt.Println(k.Sprint())
+
+	// Get parameters.
+	if err := k.Load(parameterstore.ProviderWithClient(parameterstore.Config[ssm.GetParametersInput]{
+		Delim: ".",
+		Input: ssm.GetParametersInput{Names: []string{"parent1", "parent2.child1"}, WithDecryption: aws.Bool(true)},
+	}, client), nil); err != nil {
+		log.Fatalf("error loading config: %v", err)
+	}
+	fmt.Println(k.Sprint())
+
+	// Get parameters by path.
+	if err := k.Load(parameterstore.ProviderWithClient(parameterstore.Config[ssm.GetParametersByPathInput]{
+		Delim: ".",
+		Input: ssm.GetParametersByPathInput{Path: aws.String("/"), WithDecryption: aws.Bool(true)},
+	}, client), nil); err != nil {
+		log.Fatalf("error loading config: %v", err)
+	}
+	fmt.Println(k.Sprint())
+}
