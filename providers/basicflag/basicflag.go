b0a74223ceef9cf227565e99d8c6cb06d4d4e3dc
Jan-Otto Kröpke <github@jkroepke.de>
2023-11-25 12:47:57 +0100

    basicflag: Use default value from flagset, if value is not already define

--- providers/basicflag/basicflag.go
+++ providers/basicflag/basicflag.go
@@ -9,10 +9,18 @@ import (
 	"github.com/knadh/koanf/maps"
 )
 
+// KoanfIntf is an interface that represents a small subset of methods
+// used by this package from Koanf{}. When using this package, a live
+// instance of Koanf{} should be passed.
+type KoanfIntf interface {
+	Exists(string) bool
+}
+
 // Pflag implements a pflag command line provider.
 type Pflag struct {
 	delim   string
 	flagset *flag.FlagSet
+	ko      KoanfIntf
 	cb      func(key string, value string) (string, interface{})
 }
 
@@ -21,10 +29,17 @@ type Pflag struct {
 // nesting hierarchy of keys are defined by delim. For instance, the
 // delim "." will convert the key `parent.child.key: 1`
 // to `{parent: {child: {key: 1}}}`.
-func Provider(f *flag.FlagSet, delim string) *Pflag {
-	return &Pflag{
+//
+// It takes an optional (but recommended) Koanf instance to see if the
+// the flags defined have been set from other providers, for instance,
+// a config file. If they are not, then the default values of the flags
+// are merged. If they do exist, the flag values are not merged but only
+// the values that have been explicitly set in the command line are merged.
+func Provider(f *flag.FlagSet, delim string, ko KoanfIntf) *Pflag {
+	return &flagProvider{
 		flagset: f,
 		delim:   delim,
+		ko:      ko,
 	}
 }
 
@@ -32,18 +47,25 @@ func Provider(f *flag.FlagSet, delim string) *Pflag {
 // takes a (key, value) with the variable name and value and allows you
 // to modify both. This is useful for cases where you may want to return
 // other types like a string slice instead of just a string.
-func ProviderWithValue(f *flag.FlagSet, delim string, cb func(key string, value string) (string, interface{})) *Pflag {
+//
+// It takes an optional (but recommended) Koanf instance to see if the
+// the flags defined have been set from other providers, for instance,
+// a config file. If they are not, then the default values of the flags
+// are merged. If they do exist, the flag values are not merged but only
+// the values that have been explicitly set in the command line are merged.
+func ProviderWithValue(f *flag.FlagSet, delim string, cb func(key string, value string) (string, interface{}), ko KoanfIntf) *Pflag {
 	return &Pflag{
 		flagset: f,
 		delim:   delim,
 		cb:      cb,
-	}
+		ko:      ko,
+ 	}
 }
 
 // Read reads the flag variables and returns a nested conf map.
 func (p *Pflag) Read() (map[string]interface{}, error) {
 	mp := make(map[string]interface{})
-	p.flagset.VisitAll(func(f *flag.Flag) {
+	p.flagset.Visit(func(f *flag.Flag) {
 		if p.cb != nil {
 			key, value := p.cb(f.Name, f.Value.String())
 			// If the callback blanked the key, it should be omitted
@@ -55,6 +77,32 @@ func (p *Pflag) Read() (map[string]interface{}, error) {
 			mp[f.Name] = f.Value.String()
 		}
 	})
+
+	if p.ko != nil {
+		p.flagset.VisitAll(func(f *flag.Flag) {
+			key := f.Name
+			var value interface{} = f.Value.String()
+
+			if p.cb != nil {
+				key, value = p.cb(f.Name, f.Value.String())
+				// If the callback blanked the key, it should be omitted
+				if key == "" {
+					return
+				}
+			}
+
+			if p.ko.Exists(key) {
+				return
+			}
+
+			if _, exists := mp[key]; exists {
+				return
+			}
+
+			mp[key] = value
+		})
+	}
+
 	return maps.Unflatten(mp, p.delim), nil
 }
 
