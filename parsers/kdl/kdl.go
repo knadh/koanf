4861033feaa71b4fbd158872b9ac27aae2e1f5ef
Drewry Pope <drewrypope@gmail.com>
2023-12-24 09:55:44 -0600

    nil

--- parsers/kdl/kdl.go
+++ parsers/kdl/kdl.go
@@ -42,7 +42,7 @@ func (p *KDL) Unmarshal(b []byte) (map[string]interface{}, error) {
 // Marshal marshals the given config map to KDL bytes.
 func (p *KDL) Marshal(o map[string]interface{}) ([]byte, error) {
 	if o == nil {
-		return nil, nil
+		return make([]byte, 0), nil
 	}
 	return kdl.Marshal(o)
 }
