e42d6ac3d82027c8edb04a2d265d1d8769757c0e
GreyXor <greyxor@protonmail.com>
2024-02-14 10:34:29 +0100

    feat: remove v1 toml parser

--- parsers/toml/toml.go
+++ parsers/toml/toml.go
@@ -2,9 +2,7 @@
 package toml
 
 import (
-	"bytes"
-
-	"github.com/pelletier/go-toml"
+	"github.com/pelletier/go-toml/v2"
 )
 
 // TOML implements a TOML parser.
@@ -17,18 +15,21 @@ func Parser() *TOML {
 
 // Unmarshal parses the given TOML bytes.
 func (p *TOML) Unmarshal(b []byte) (map[string]interface{}, error) {
-	r, err := toml.LoadReader(bytes.NewBuffer(b))
-	if err != nil {
+	var outMap map[string]interface{}
+
+	if err := toml.Unmarshal(b, &outMap); err != nil {
 		return nil, err
 	}
-	return r.ToMap(), err
+
+	return outMap, nil
 }
 
 // Marshal marshals the given config map to TOML bytes.
 func (p *TOML) Marshal(o map[string]interface{}) ([]byte, error) {
-	out, err := toml.TreeFromMap(o)
+	out, err := toml.Marshal(&o)
 	if err != nil {
 		return nil, err
 	}
-	return out.Marshal()
+
+	return out, nil
 }
