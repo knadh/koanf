3a90fd9c191802661fbb33daa2093ddc12f8674e
Kailash Nadh <kailash@nadh.in>
2025-06-29 11:24:56 +0530

    Refactor env tests.
    - Switch to the `f()` pattern with an arg struct for better readability.
    - Add new test for non-string param value.

--- providers/env/env_test.go
+++ providers/env/env_test.go
@@ -1,143 +1,50 @@
 package env
 
 import (
-	"os"
 	"slices"
+	"strconv"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestProvider(t *testing.T) {
-	mockEnviron := func() []string {
-		return []string{"TEST_FOO=bar"}
-	}
-	xformSimple := func(k, v string) (string, any) {
-		return strings.ToLower(k), v
-	}
-	xformTwo := func(k, v string) (string, any) {
-		return strings.ReplaceAll(strings.ToUpper(k), "_", "."), v
-	}
+func mockEnviron() []string {
+	return []string{"TEST_FOO=bar"}
+}
 
-	testCases := []struct {
+func TestProvider(t *testing.T) {
+	type Args struct {
 		name     string
-		prefix   string
 		delim    string
+		opt      Opt
 		key      string
 		value    string
 		expKey   string
 		expValue string
-		opt      Opt
-		want     *Env
-	}{
-		{
-			name:  "Nil TransformFunc",
-			delim: ".",
-			opt:   Opt{Prefix: "TESTVAR_"},
-			want: &Env{
-				prefix:  "TESTVAR_",
-				delim:   ".",
-				environ: os.Environ,
-			},
-		},
-		{
-			name:     "Simple TransformFunc",
-			delim:    ".",
-			key:      "TestKey",
-			value:    "TestVal",
-			expKey:   "testkey",
-			expValue: "TestVal",
-			opt: Opt{
-				Prefix:        "TESTVAR_",
-				TransformFunc: xformSimple,
-			},
-			want: &Env{
-				prefix:  "TESTVAR_",
-				delim:   ".",
-				environ: os.Environ,
-			},
-		},
-		{
-			name:  "Empty options",
-			delim: ".",
-			opt:   Opt{},
-			want: &Env{
-				delim:   ".",
-				environ: os.Environ,
-			},
-		},
-		{
-			name:     "TransformFunc is given, no Prefix",
-			delim:    ".",
-			key:      "test_key",
-			value:    "test_val",
-			expKey:   "TEST.KEY",
-			expValue: "test_val",
-			opt: Opt{
-				TransformFunc: xformTwo,
-			},
-			want: &Env{
-				delim:   ".",
-				environ: os.Environ,
-			},
-		},
-		{
-			name:     "Custom cb function",
-			delim:    ".",
-			key:      "TEST_KEY",
-			value:    "test_val",
-			expKey:   "key",
-			expValue: "prod_val",
-			opt: Opt{
-				Prefix: "TEST_",
-				TransformFunc: func(key string, value string) (string, any) {
-					key = strings.ReplaceAll(strings.TrimPrefix(strings.ToLower(key), "test_"), "_", ".")
-					value = strings.ReplaceAll(value, "test", "prod")
-					return key, value
-				},
-			},
-			want: &Env{
-				prefix:  "TEST_",
-				delim:   ".",
-				environ: os.Environ,
-			},
-		},
-		{
-			name:  "Environ func is given",
-			delim: ".",
-			opt: Opt{
-				Prefix:      "TEST_",
-				EnvironFunc: mockEnviron,
-			},
-			want: &Env{
-				prefix:  "TEST_",
-				delim:   ".",
-				environ: mockEnviron,
-			},
-		},
+		wantOpt  Opt // Expected options for comparison.
 	}
+	f := func(args Args) {
+		t.Run(args.name, func(t *testing.T) {
+			gotProvider := Provider(args.delim, args.opt)
+			wantProvider := Provider(args.delim, args.wantOpt)
 
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			gotProvider := Provider(tc.delim, tc.opt)
+			// Config variables.
+			assert.Equal(t, wantProvider.delim, gotProvider.delim)
+			assert.Equal(t, wantProvider.prefix, gotProvider.prefix)
 
-			// Member variables.
-			// Cannot use assert.Equal for the Env itself due to the presense
-			// of function member variables.
-			assert.Equal(t, tc.want.delim, gotProvider.delim)
-			assert.Equal(t, tc.want.prefix, gotProvider.prefix)
-
-			// TransformFunc
-			if tc.opt.TransformFunc != nil {
-				k, v := gotProvider.transform(tc.key, tc.value)
-				assert.Equal(t, tc.expKey, k)
-				assert.Equal(t, tc.expValue, v)
+			// Optioonal TransformFunc.
+			if args.opt.TransformFunc != nil {
+				k, v := gotProvider.transform(args.key, args.value)
+				assert.Equal(t, args.expKey, k)
+				assert.Equal(t, args.expValue, v)
 			}
 
-			// EnvironFunc
-			wantEnv := tc.want.environ()
-			gotEnv := gotProvider.environ()
+			// Optional EnvironFunc.
+			var (
+				wantEnv = wantProvider.environ()
+				gotEnv  = gotProvider.environ()
+			)
 			slices.Sort(wantEnv)
 			slices.Sort(gotEnv)
 			if !slices.Equal(wantEnv, gotEnv) {
@@ -146,117 +53,113 @@ func TestProvider(t *testing.T) {
 			}
 		})
 	}
+
+	// Execute the test cases.
+	f(Args{name: "No TransformFunc", delim: ".", opt: Opt{Prefix: "TESTVAR_"}, wantOpt: Opt{Prefix: "TESTVAR_"}})
+	f(Args{name: "No Opt", delim: "."})
+	f(Args{name: "Has EnvironFunc", delim: ".", opt: Opt{Prefix: "TEST_", EnvironFunc: mockEnviron}, wantOpt: Opt{Prefix: "TEST_", EnvironFunc: mockEnviron}})
+	f(Args{
+		name: "Has TransformFunc", delim: ".", key: "TestKey", value: "TestVal", expKey: "testkey", expValue: "TestVal", opt: Opt{Prefix: "TESTVAR_", TransformFunc: func(k, v string) (string, any) {
+			return strings.ToLower(k), v
+		}},
+		wantOpt: Opt{Prefix: "TESTVAR_"},
+	})
+	f(Args{
+		name: "Has TransformFunc. No Prefix", delim: ".", key: "test_key", value: "test_val", expKey: "TEST.KEY", expValue: "test_val", opt: Opt{
+			TransformFunc: func(k, v string) (string, any) {
+				return strings.ReplaceAll(strings.ToUpper(k), "_", "."), v
+			}},
+		wantOpt: Opt{},
+	})
+	f(Args{
+		name: "Has TransformFunc", delim: ".", key: "TEST_KEY", value: "test_val", expKey: "key", expValue: "prod_val", wantOpt: Opt{Prefix: "TEST_"}, opt: Opt{
+			Prefix: "TEST_",
+			TransformFunc: func(key string, value string) (string, any) {
+				key = strings.ReplaceAll(strings.TrimPrefix(strings.ToLower(key), "test_"), "_", ".")
+				value = strings.ReplaceAll(value, "test", "prod")
+				return key, value
+			},
+		},
+	})
+
 }
 
 func TestRead(t *testing.T) {
-	testCases := []struct {
+	type Args struct {
 		name     string
 		key      string
 		value    string
 		expKey   string
-		expValue string
-		env      *Env
-	}{
-		{
-			name:     "No cb",
-			key:      "TEST_KEY",
-			value:    "TEST_VAL",
-			expKey:   "TEST_KEY",
-			expValue: "TEST_VAL",
-			env: &Env{
-				delim:   ".",
-				environ: os.Environ,
-			},
-		},
-		{
-			name:     "cb given",
-			key:      "TEST_KEY",
-			value:    "TEST_VAL",
-			expKey:   "test.key",
-			expValue: "TEST_VAL",
-			env: &Env{
-				delim: "_",
-				transform: func(key string, value string) (string, any) {
-					return strings.ReplaceAll(strings.ToLower(key), "_", "."), value
-				},
-				environ: os.Environ,
-			},
-		},
-		{
-			name:     "No cb - prefix given",
-			key:      "TEST_KEY",
-			value:    "TEST_VAL",
-			expKey:   "test.key",
-			expValue: "TEST_VAL",
-			env: &Env{
-				prefix: "TEST",
-				delim:  "/",
-				transform: func(key string, value string) (string, any) {
-					return strings.ReplaceAll(strings.ToLower(key), "_", "."), value
-				},
-				environ: os.Environ,
-			},
-		},
-		{
-			name:     "Path value",
-			key:      "TEST_DIR",
-			value:    "/test/dir/file",
-			expKey:   "TEST_DIR",
-			expValue: "/test/dir/file",
-			env: &Env{
-				delim:   ".",
-				environ: os.Environ,
-			},
-		},
-		{
-			name:     "Replace value with underscore",
-			key:      "TEST_DIR",
-			value:    "/test/dir/file",
-			expKey:   "TEST_DIR",
-			expValue: "_test_dir_file",
-			env: &Env{
-				delim: ".",
-				transform: func(key string, value string) (string, any) {
-					return key, strings.ReplaceAll(strings.ToLower(value), "/", "_")
-				},
-				environ: os.Environ,
-			},
-		},
-		{
-			name:     "Empty value",
-			key:      "KEY",
-			value:    "",
-			expKey:   "KEY",
-			expValue: "",
-			env: &Env{
-				delim:   ".",
-				environ: os.Environ,
-			},
-		},
-		{
-			name:     "Environ func provided",
-			key:      "TEST_KEY",
-			value:    "TEST_VAL",
-			expKey:   "TEST_OVERRIDE_KEY",
-			expValue: "TEST_OVERRIDE_VAL",
-			env: &Env{
-				delim: ".",
-				environ: func() []string {
-					return []string{"TEST_OVERRIDE_KEY=TEST_OVERRIDE_VAL"}
-				},
-			},
-		},
+		expValue any
+		opt      Opt
+		delim    string
 	}
 
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			t.Setenv(tc.key, tc.value)
+	f := func(args Args) {
+		t.Run(args.name, func(t *testing.T) {
+			t.Setenv(args.key, args.value)
 
-			envs, err := tc.env.Read()
+			env := Provider(args.delim, args.opt)
+			envs, err := env.Read()
 			assert.Nil(t, err)
-			v, ok := envs[tc.expKey]
+
+			v, ok := envs[args.expKey]
 			assert.True(t, ok)
-			assert.Equal(t, tc.expValue, v)
+			assert.Equal(t, args.expValue, v)
 		})
 	}
+
+	// Execute the test cases.
+	f(Args{name: "No TransformFunc", key: "TEST_KEY", value: "TEST_VAL", expKey: "TEST_KEY", expValue: "TEST_VAL", delim: ".", opt: Opt{}})
+	f(Args{name: "Path value", key: "TEST_DIR", value: "/test/dir/file", expKey: "TEST_DIR", expValue: "/test/dir/file", delim: "."})
+	f(Args{name: "Empty value", key: "KEY", value: "", expKey: "KEY", expValue: "", delim: ".", opt: Opt{}})
+	f(Args{
+		name: "Has TransformFunc", key: "TEST_KEY", value: "TEST_VAL", expKey: "test.key", expValue: "TEST_VAL", delim: "_", opt: Opt{
+			TransformFunc: func(key string, value string) (string, any) {
+				return strings.ReplaceAll(strings.ToLower(key), "_", "."), value
+			},
+		},
+	})
+	f(Args{
+		name: "No TransformFunc. Has Prefix.", key: "TEST_KEY", value: "TEST_VAL", expKey: "test.key", expValue: "TEST_VAL", delim: "/",
+		opt: Opt{
+			Prefix: "TEST",
+			TransformFunc: func(key string, value string) (string, any) {
+				return strings.ReplaceAll(strings.ToLower(key), "_", "."), value
+			},
+		},
+	})
+	f(Args{
+		name: "Transform key", key: "TEST_DIR", value: "/test/dir/file", expKey: "TEST_DIR", expValue: "_test_dir_file", delim: ".",
+		opt: Opt{
+			TransformFunc: func(key string, value string) (string, any) {
+				return key, strings.ReplaceAll(strings.ToLower(value), "/", "_")
+			},
+		},
+	})
+	f(Args{
+		name: "Has TransformFunc", key: "TEST_KEY", value: "TEST_VAL", expKey: "TEST_OVERRIDE_KEY", expValue: "TEST_OVERRIDE_VAL", delim: ".", opt: Opt{
+			EnvironFunc: func() []string {
+				return []string{"TEST_OVERRIDE_KEY=TEST_OVERRIDE_VAL"}
+			},
+		},
+	})
+	f(Args{
+		name: "Has int value", key: "TEST_KEY", value: "123", expKey: "test_key", expValue: 123, delim: ".", opt: Opt{
+			EnvironFunc: func() []string {
+				return []string{"TEST_KEY=123"}
+			},
+			TransformFunc: func(key string, value string) (string, any) {
+				if key == "TEST_KEY" {
+					intval, err := strconv.Atoi(value)
+					if err != nil {
+						return "", nil
+					}
+					return "test_key", intval
+				}
+
+				return "", ""
+			},
+		},
+	})
 }
